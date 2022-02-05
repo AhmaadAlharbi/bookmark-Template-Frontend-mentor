f7719c7734ca0cf1acb34eb3d06ee1f89c83eb23
ahmadZaid <ahmaadzaid7@gmail.com>
2022-02-05 19:09:45 +0300

    toggle FAQ

--- script.js
+++ script.js
@@ -29,3 +29,21 @@ function openTab(evt, contentId) {
   evt.currentTarget.className += " show";
   evt.currentTarget.className += " active";
 }
+
+const question = document.getElementsByClassName("question");
+
+for (i = 0; i < question.length; i++) {
+  question[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var answer = this.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+}
